43c46bc142fc2b7c31967d996594f91fe3bc7784
aahmed0520 <aahmed19@ucsc.edu>
2025-06-06 22:36:15 -0700

    First Commit

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,49 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  planetName: ["Glorvax-9", "Zentha Prime", "Orbital Drift Delta", "Xeelee's Cradle"],
+  attraction: ["floating lava gardens", "zero-gravity coral reefs", "time-loop markets", "bioluminescent ice caves", "gravity-inverted jungles"],
+  worldType: ["crystal-based hive world", "sentient oceanic planet", "bioluminescent gas world", "modular asteroid habitat"],
+  creature: ["singing crystal crabs", "hyperintelligent fungi", "three-eyed space lemurs", "orb-weaving data spiders"],
+  transport: ["telekinetic gondola", "hover-ferry", "biopod taxi", "thought-propelled scooter"],
+  landmark: ["Temple of Ten Thousand Screams", "Hollow Moon Observatory", "Whispering Nebula Spa", "Skybones Cathedral"],
+  event: ["quantum fire opera", "telepathy slam poetry", "plasma-surfing ritual", "chronowarp symphony"],
+  souvenir: ["transdimensional pollen", "fractal coin", "liquid magnet souvenir", "glowing antimatter trinket"]
+};
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const template = `
+Welcome to $planetName!
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+Experience the $attraction of this $worldType, home to the $creature.
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
+Our local guides will escort you via $transport to the $landmark, where you can witness a live $event.
+
+Book now and receive a free vial of $souvenir!
+`;
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+}
+
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
   }
+
+  // Use jQuery to update the HTML on your portfolio site
+  $("#box").text(story);
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  $("#clicker").click(generate);
+  generate(); // run once on page load
 }
 
-// let's get this party started - uncomment me
-//main();
+// Start the generator
+main();
