889336f113f08f7b8b846121dc4c63c2c988c9ca
aahmed0520 <aahmed19@ucsc.edu>
2025-06-08 03:16:37 -0700

    Final Changes on Display

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -37,7 +37,7 @@ function draw() {
   background(10, 10, 20); // dark night sky
 
   // Animated aurora bands using sampled shades
-  const numBands = 100;
+  const numBands = 125;
   for (let i = 0; i < numBands; i++) {
     let baseX = width * random();
     let waveOffset = 30 * sin(millis() / 2000 + i);
@@ -59,7 +59,7 @@ function draw() {
   const treeCount = 10;
   for (let i = 0; i < treeCount; i++) {
     let x = random(width);
-    let y = height / 2 + 10 + random(10);
+    let y = height / 2 + 40 + random(10);
     let s = random(60,80) ;
     triangle(x, y - s, x - s / 3, y, x + s / 3, y);
   }
