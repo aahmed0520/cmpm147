860683369c4060db4ecb0b6733769ee4f2d9a7bb
aahmed0520 <aahmed19@ucsc.edu>
2025-06-10 19:04:41 -0700

    Update sketch_desert.js

--- experiment4/js/sketch_desert.js
+++ experiment4/js/sketch_desert.js
@@ -1,146 +1,132 @@
-new p5((sketch) => {
-    "use strict";
+window.addEventListener("load", () => {
+    new p5((sketch) => {
+      "use strict";
   
-    /* global XXH */
+      /* global XXH */
   
-    // These functions get used by engine.js
-    sketch.preload = function () {
-      window.p3_preload?.();
-    };
+      window.p3_preload = function () {};
   
-    sketch.setup = function () {
-      window.p3_setup?.();
-    };
+      window.p3_setup = function () {};
   
-    sketch.draw = function () {
-      // noop – draw is handled by engine.js
-    };
+      window.p3_tileWidth = () => 32;
+      window.p3_tileHeight = () => 16;
   
-    sketch.mouseClicked = function () {
-      window.mouseClicked?.(); // engine.js defines mouseClicked globally
-      return false;
-    };
+      let [tw, th] = [window.p3_tileWidth(), window.p3_tileHeight()];
+      let worldSeed;
+      let clicks = {};
   
-    window.p3_tileWidth = () => 32;
-    window.p3_tileHeight = () => 16;
+      window.p3_worldKeyChanged = function (key) {
+        worldSeed = XXH.h32(key, 0);
+        sketch.noiseSeed(worldSeed);
+        sketch.randomSeed(worldSeed);
+      };
   
-    let [tw, th] = [window.p3_tileWidth(), window.p3_tileHeight()];
-    let worldSeed;
-    let clicks = {};
+      window.p3_tileClicked = function (i, j) {
+        let key = [i, j];
+        clicks[key] = sketch.millis(); // store time of click
+      };
   
-    window.p3_worldKeyChanged = function (key) {
-      worldSeed = XXH.h32(key, 0);
-      sketch.noiseSeed(worldSeed);
-      sketch.randomSeed(worldSeed);
-    };
+      window.p3_drawBefore = function () {};
   
-    window.p3_setup = function () {};
+      window.p3_drawTile = function (i, j) {
+        let n = sketch.noise(i * 0.1, j * 0.1);
+        let rockColor = sketch.color('#8AAAB2');
+        let dustColor = sketch.color('#CFCFC6');
+        let rustColor = sketch.color('#B86D46');
+        let duneColor = sketch.color('#6B3E2E');
   
-    window.p3_tileClicked = function (i, j) {
-      let key = [i, j];
-      clicks[key] = sketch.millis(); // store time of click
-    };
+        if (n < 0.35) sketch.fill(dustColor);
+        else if (n < 0.55) sketch.fill(rustColor);
+        else if (n < 0.75) sketch.fill(rockColor);
+        else sketch.fill(duneColor);
   
-    window.p3_drawBefore = function () {};
+        sketch.push();
   
-    window.p3_drawTile = function (i, j) {
-      let n = sketch.noise(i * 0.1, j * 0.1);
-      let rockColor = sketch.color("#8AAAB2");
-      let dustColor = sketch.color("#CFCFC6");
-      let rustColor = sketch.color("#B86D46");
-      let duneColor = sketch.color("#6B3E2E");
+        let h = (sketch.noise(i * 0.15 + 100, j * 0.15 + 100) - 0.5) * 12;
+        let top = (sketch.noise(i + 1, j + 1) - 0.5) * 10;
+        let right = (sketch.noise(i + 2, j) - 0.5) * 10;
+        let bottom = (sketch.noise(i - 1, j - 2) - 0.5) * 10;
+        let left = (sketch.noise(i, j + 3) - 0.5) * 10;
   
-      if (n < 0.35) sketch.fill(dustColor);
-      else if (n < 0.55) sketch.fill(rustColor);
-      else if (n < 0.75) sketch.fill(rockColor);
-      else sketch.fill(duneColor);
+        // Shadow
+        sketch.push();
+        sketch.noStroke();
+        sketch.fill(0, 0, 0, 40);
+        sketch.beginShape();
+        sketch.vertex(-tw + 2, 0 + left + h + 4);
+        sketch.vertex(0 + 2, th + bottom + h + 4);
+        sketch.vertex(tw + 2, 0 + right + h + 4);
+        sketch.vertex(0 + 2, -th + top + h + 4);
+        sketch.endShape(sketch.CLOSE);
+        sketch.pop();
   
-      sketch.push();
+        // Main shape
+        sketch.noStroke();
+        sketch.beginShape();
+        sketch.vertex(-tw, 0 + left + h);
+        sketch.vertex(0, th + bottom + h);
+        sketch.vertex(tw, 0 + right + h);
+        sketch.vertex(0, -th + top + h);
+        sketch.endShape(sketch.CLOSE);
   
-      let h = (sketch.noise(i * 0.15 + 100, j * 0.15 + 100) - 0.5) * 12;
-      let top = (sketch.noise(i + 1, j + 1) - 0.5) * 10;
-      let right = (sketch.noise(i + 2, j) - 0.5) * 10;
-      let bottom = (sketch.noise(i - 1, j - 2) - 0.5) * 10;
-      let left = (sketch.noise(i, j + 3) - 0.5) * 10;
+        // Dune ripple animation
+        if (n >= 0.75) {
+          sketch.stroke(60, 30, 20, 180);
+          sketch.strokeWeight(1);
+          sketch.noFill();
+          let t = sketch.millis() * 0.0002;
   
-      // Shadow
-      sketch.push();
-      sketch.noStroke();
-      sketch.fill(0, 0, 0, 40);
-      sketch.beginShape();
-      sketch.vertex(-tw + 2, 0 + left + h + 4);
-      sketch.vertex(0 + 2, th + bottom + h + 4);
-      sketch.vertex(tw + 2, 0 + right + h + 4);
-      sketch.vertex(0 + 2, -th + top + h + 4);
-      sketch.endShape(sketch.CLOSE);
-      sketch.pop();
-  
-      // Main shape
-      sketch.noStroke();
-      sketch.beginShape();
-      sketch.vertex(-tw, 0 + left + h);
-      sketch.vertex(0, th + bottom + h);
-      sketch.vertex(tw, 0 + right + h);
-      sketch.vertex(0, -th + top + h);
-      sketch.endShape(sketch.CLOSE);
-  
-      // Dune ripple animation
-      if (n >= 0.75) {
-        sketch.stroke(60, 30, 20, 180);
-        sketch.strokeWeight(1);
-        sketch.noFill();
-        let t = sketch.millis() * 0.0002;
-  
-        for (let r = -th + 4; r < th - 4; r += 6) {
-          sketch.beginShape();
-          for (let x = -tw + 2; x <= tw - 2; x += 4) {
-            let wave = Math.sin((i + x) * 0.3 + (j + r) * 0.2 + t) * 2;
-            let y = r + wave;
-            sketch.curveVertex(x, y + h);
+          for (let r = -th + 4; r < th - 4; r += 6) {
+            sketch.beginShape();
+            for (let x = -tw + 2; x <= tw - 2; x += 4) {
+              let wave = sketch.sin((i + x) * 0.3 + (j + r) * 0.2 + t) * 2;
+              let y = r + wave;
+              sketch.curveVertex(x, y + h);
+            }
+            sketch.endShape();
           }
-          sketch.endShape();
         }
-      }
   
-      // Rock cracks
-      if (n >= 0.55 && n < 0.75) {
-        sketch.stroke(30, 30, 30, 80);
-        sketch.strokeWeight(0.5);
-        sketch.randomSeed(XXH.h32("crack:" + [i, j], worldSeed));
-        for (let k = 0; k < 2; k++) {
-          let x1 = sketch.random(-tw * 0.8, tw * 0.8);
-          let y1 = sketch.random(-th, th);
-          let x2 = x1 + sketch.random(-5, 5);
-          let y2 = y1 + sketch.random(-5, 5);
-          sketch.line(x1, y1 + h, x2, y2 + h);
+        // Rock cracks
+        if (n >= 0.55 && n < 0.75) {
+          sketch.stroke(30, 30, 30, 80);
+          sketch.strokeWeight(0.5);
+          sketch.randomSeed(XXH.h32("crack:" + [i, j], worldSeed));
+          for (let k = 0; k < 2; k++) {
+            let x1 = sketch.random(-tw * 0.8, tw * 0.8);
+            let y1 = sketch.random(-th, th);
+            let x2 = x1 + sketch.random(-5, 5);
+            let y2 = y1 + sketch.random(-5, 5);
+            sketch.line(x1, y1 + h, x2, y2 + h);
+          }
         }
-      }
   
-      // Click glow
-      let t = clicks[[i, j]];
-      if (t !== undefined) {
-        let pulse = Math.sin((sketch.millis() - t) / 300.0) * 5 + 7;
-        sketch.fill(200, 255, 255, 180);
-        sketch.ellipse(0, 0 + h, pulse, pulse / 2);
-      }
+        // Click glow
+        let t = clicks[[i, j]];
+        if (t !== undefined) {
+          let pulse = sketch.sin((sketch.millis() - t) / 300.0) * 5 + 7;
+          sketch.fill(200, 255, 255, 180);
+          sketch.ellipse(0, 0 + h, pulse, pulse / 2);
+        }
   
-      sketch.pop();
-    };
+        sketch.pop();
+      };
   
-    window.p3_drawSelectedTile = function (i, j) {
-      sketch.noFill();
-      sketch.stroke(0, 255, 0, 128);
-      sketch.beginShape();
-      sketch.vertex(-tw, 0);
-      sketch.vertex(0, th);
-      sketch.vertex(tw, 0);
-      sketch.vertex(0, -th);
-      sketch.endShape(sketch.CLOSE);
-      sketch.noStroke();
-      sketch.fill(0);
-      sketch.text("tile " + [i, j], 0, 0);
-    };
+      window.p3_drawSelectedTile = function (i, j) {
+        sketch.noFill();
+        sketch.stroke(0, 255, 0, 128);
+        sketch.beginShape();
+        sketch.vertex(-tw, 0);
+        sketch.vertex(0, th);
+        sketch.vertex(tw, 0);
+        sketch.vertex(0, -th);
+        sketch.endShape(sketch.CLOSE);
+        sketch.noStroke();
+        sketch.fill(0);
+        sketch.text("tile " + [i, j], 0, 0);
+      };
   
-    window.p3_drawAfter = function () {};
-  }, "canvas-container");
+      window.p3_drawAfter = function () {};
+    }, "canvas-container");
+  });
   
