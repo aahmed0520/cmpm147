01b7b4db39026472962a9f669752dcac142d419a
aahmed0520 <aahmed19@ucsc.edu>
2025-06-11 21:29:53 -0700

    Update mydesign.js

--- experiment5/js/mydesign.js
+++ experiment5/js/mydesign.js
@@ -1,107 +1,101 @@
-let inspirations = getInspirations();
-
-function preload() {
-  for (let insp of inspirations) {
-    insp.image = loadImage(insp.assetUrl);
-  }
-}
-
 function getInspirations() {
-  return [
-    {
-      name: "Pokeball",
-      assetUrl: "img/pokeball.webp",
-      credit: "Pokémon"
-    },
-    {
-      name: "Naruto",
-      assetUrl: "img/Naruto.jpg",
-      credit: "Masashi Kishimoto"
-    },
-    {
-      name: "Pikachu",
-      assetUrl: "img/pikachu.jpg.svg",
-      credit: "Pokémon"
+    return [
+      {
+        name: "Pokeball",
+        assetUrl: "img/pokeball.webp",
+        credit: "Pokémon"
+      },
+      {
+        name: "Naruto",
+        assetUrl: "img/Naruto.jpg",
+        credit: "Masashi Kishimoto"
+      },
+      {
+        name: "Pikachu",
+        assetUrl: "img/pikachu.jpg.svg",
+        credit: "Pokémon"
+      }
+    ];
+  }
+  
+  let inspirations = getInspirations();
+  
+  function preload() {
+    for (let insp of inspirations) {
+      insp.image = loadImage(insp.assetUrl);
     }
-  ];
-}
-
-function initDesign(inspiration) {
-  resizeCanvas(inspiration.image.width / 4, inspiration.image.height / 4);
-
-  let design = {
-    name: inspiration.name,
-    elements: []
-  };
-
-  if (inspiration.name === "Pokeball") {
-    design.shape = "circle";
-    design.count = 10;
-  } else if (inspiration.name === "Naruto") {
-    design.shape = "line";
-    design.count = 20;
-  } else if (inspiration.name === "Pikachu") {
-    design.shape = "triangle";
-    design.count = 15;
   }
-
-  for (let i = 0; i < design.count; i++) {
-    design.elements.push({
-      x: random(width),
-      y: random(height),
-      size: random(10, 50),
-      angle: random(TWO_PI)
-    });
-  }
-
-  return design;
-}
-
-function renderDesign(design, inspiration) {
-  background(255);
-  noStroke();
-  fill(0);
-
-  for (let elem of design.elements) {
-    push();
-    translate(elem.x, elem.y);
-    rotate(elem.angle);
-
-    if (design.shape === "circle") {
-      ellipse(0, 0, elem.size, elem.size);
-    } else if (design.shape === "line") {
-      stroke(0);
-      line(-elem.size / 2, 0, elem.size / 2, 0);
-      noStroke();
-    } else if (design.shape === "triangle") {
-      triangle(
-        -elem.size / 2, elem.size / 2,
-        0, -elem.size / 2,
-        elem.size / 2, elem.size / 2
-      );
+  
+  function initDesign(inspiration) {
+    resizeCanvas(inspiration.image.width / 4, inspiration.image.height / 4);
+  
+    let design = {
+      name: inspiration.name,
+      elements: []
+    };
+  
+    if (inspiration.name === "Pokeball") {
+      design.shape = "circle";
+      design.count = 10;
+    } else if (inspiration.name === "Naruto") {
+      design.shape = "line";
+      design.count = 20;
+    } else if (inspiration.name === "Pikachu") {
+      design.shape = "triangle";
+      design.count = 15;
     }
-
-    pop();
+  
+    for (let i = 0; i < design.count; i++) {
+      design.elements.push({
+        x: random(width),
+        y: random(height),
+        size: random(10, 50),
+        angle: random(TWO_PI)
+      });
+    }
+  
+    return design;
   }
-  function mutateDesign(design, inspiration, rate) {
-    // rate is from 0.01 to 1.0 (slider)
-    let mutationAmount = rate / 100;
+  
+  function renderDesign(design, inspiration) {
+    background(255);
+    noStroke();
+    fill(0);
   
     for (let elem of design.elements) {
-      // Slightly move positions
-      elem.x = mut(elem.x, 0, width, mutationAmount);
-      elem.y = mut(elem.y, 0, height, mutationAmount);
+      push();
+      translate(elem.x, elem.y);
+      rotate(elem.angle);
   
-      // Slightly change size
-      elem.size = mut(elem.size, 10, 100, mutationAmount);
+      if (design.shape === "circle") {
+        ellipse(0, 0, elem.size, elem.size);
+      } else if (design.shape === "line") {
+        stroke(0);
+        line(-elem.size / 2, 0, elem.size / 2, 0);
+        noStroke();
+      } else if (design.shape === "triangle") {
+        triangle(
+          -elem.size / 2, elem.size / 2,
+          0, -elem.size / 2,
+          elem.size / 2, elem.size / 2
+        );
+      }
   
-      // Slightly change angle
-      elem.angle = mut(elem.angle, 0, TWO_PI, mutationAmount);
+      pop();
     }
   }
   
-  // Gaussian mutation helper
+  // ✅ These MUST be OUTSIDE renderDesign
+  function mutateDesign(design, inspiration, rate) {
+    for (let elem of design.elements) {
+      elem.x = mut(elem.x, 0, width, rate);
+      elem.y = mut(elem.y, 0, height, rate);
+      elem.size = mut(elem.size, 10, 100, rate);
+      elem.angle = mut(elem.angle, 0, TWO_PI, rate);
+    }
+  }
+  
   function mut(num, min, max, rate) {
     return constrain(randomGaussian(num, (rate * (max - min)) / 20), min, max);
   }
-}
+  
