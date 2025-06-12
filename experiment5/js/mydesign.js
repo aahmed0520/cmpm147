ca89f38eecaa852d3625bc9ca154898ae8f9e77b
aahmed0520 <aahmed19@ucsc.edu>
2025-06-11 22:27:55 -0700

    Update mydesign.js

--- experiment5/js/mydesign.js
+++ experiment5/js/mydesign.js
@@ -39,29 +39,35 @@ function getInspirations() {
     for (let i = 0; i < count; i++) {
       let x = random(width);
       let y = random(height);
-      let shape = random(["circle", "square"]);
       let size = random(10, 40);
       let angle = random(TWO_PI);
+      let shape = random(["triangle", "square"]);
       let fill = { r: 0, g: 0, b: 0 };
   
       if (inspiration.name === "Pikachu") {
         fill = { r: 255, g: 220, b: 0 };
   
       } else if (inspiration.name === "Pokeball") {
-        // Top half red
         if (y < height / 2 - 10) {
-          fill = { r: 230, g: 0, b: 0 };
-        }
-        // Bottom half white
-        else if (y > height / 2 + 10) {
-          fill = { r: 255, g: 255, b: 255 };
-        }
-        // Center band black
-        else {
-          fill = { r: 20, g: 20, b: 20 };
-          shape = "square"; // mostly black band is flat-ish
+          fill = { r: 230, g: 0, b: 0 }; // red
+        } else if (y > height / 2 + 10) {
+          fill = { r: 255, g: 255, b: 255 }; // white
+        } else {
+          fill = { r: 20, g: 20, b: 20 }; // black band
+          shape = "square";
           size = random(10, 30);
         }
+  
+      } else if (inspiration.name === "Naruto") {
+        // Blend of Naruto-inspired color zones
+        let palette = [
+          { r: 255, g: 165, b: 0 },   // orange
+          { r: 240, g: 200, b: 160 }, // skin
+          { r: 50, g: 50, b: 50 },    // black/dark gray
+          { r: 170, g: 170, b: 170 }, // silver
+          { r: 30, g: 120, b: 200 }   // blue
+        ];
+        fill = random(palette);
       }
   
       design.elements.push({ x, y, size, angle, shape, fill });
@@ -125,6 +131,12 @@ function getInspirations() {
           elem.fill.g = mut(elem.fill.g, 0, 50, rate);
           elem.fill.b = mut(elem.fill.b, 0, 50, rate);
         }
+      }
+  
+      if (inspiration.name === "Naruto") {
+        elem.fill.r = mut(elem.fill.r, 30, 255, rate);
+        elem.fill.g = mut(elem.fill.g, 30, 220, rate);
+        elem.fill.b = mut(elem.fill.b, 30, 210, rate);
       }
     }
   }
