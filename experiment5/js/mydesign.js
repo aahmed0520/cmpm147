a643aa78ab9dbe677de70a1b0d6855ed325802c2
aahmed0520 <aahmed19@ucsc.edu>
2025-06-11 21:16:46 -0700

    steps 3 done

--- experiment5/js/mydesign.js
+++ experiment5/js/mydesign.js
@@ -1,78 +1,86 @@
-/* exported p4_inspirations, p4_initialize, p4_render, p4_mutate */
+let inspirations = getInspirations();
 
+function preload() {
+  for (let insp of inspirations) {
+    insp.image = loadImage(insp.assetUrl);
+  }
+}
 
 function getInspirations() {
-    return [
-      {
-        name: "Naruto", 
-        assetUrl: "img/Naruto.jpg",
-        credit: "Lunch atop a Skyscraper, Charles Clyde Ebbets, 1932"
-      },
-      {
-        name: "Pikachu", 
-        assetUrl: "img/pikachu.jpg.svg",
-        credit: "Train Wreck At Monteparnasse, Levy & fils, 1895"
-      },
-      {
-        name: "Pokeball", 
-        assetUrl: "img/pokeball.webp",
-        credit: "Migrant Mother near Nipomo, California, Dorothea Lange, 1936"
-      },
-    ];
-  }
-  
-  function initDesign(inspiration) {
-    // set the canvas size based on the container
-    let canvasContainer = $('.image-container'); // Select the container using jQuery
-    let canvasWidth = canvasContainer.width(); // Get the width of the container
-    let aspectRatio = inspiration.image.height / inspiration.image.width;
-    let canvasHeight = canvasWidth * aspectRatio; // Calculate the height based on the aspect ratio
-    resizeCanvas(canvasWidth, canvasHeight);
-    $(".caption").text(inspiration.credit); // Set the caption text
-  
-    // add the original image to #original
-    const imgHTML = `<img src="${inspiration.assetUrl}" style="width:${canvasWidth}px;">`
-    $('#original').empty();
-    $('#original').append(imgHTML);
-  
-    
-    let design = {
-      bg: 128,
-      fg: []
+  return [
+    {
+      name: "Pokeball",
+      assetUrl: "assets/pokeball.webp",
+      credit: "Pokémon"
+    },
+    {
+      name: "Naruto",
+      assetUrl: "assets/Naruto.jpg",
+      credit: "Masashi Kishimoto"
+    },
+    {
+      name: "Pikachu",
+      assetUrl: "assets/pikachu.jpg.svg",
+      credit: "Pokémon"
     }
-    
-    for(let i = 0; i < 100; i++) {
-      design.fg.push({x: random(width),
-                      y: random(height),
-                      w: random(width/2),
-                      h: random(height/2),
-                      fill: random(255)})
+  ];
+}
+
+function initDesign(inspiration) {
+  resizeCanvas(inspiration.image.width / 4, inspiration.image.height / 4);
+
+  let design = {
+    name: inspiration.name,
+    elements: []
+  };
+
+  if (inspiration.name === "Pokeball") {
+    design.shape = "circle";
+    design.count = 10;
+  } else if (inspiration.name === "Naruto") {
+    design.shape = "line";
+    design.count = 20;
+  } else if (inspiration.name === "Pikachu") {
+    design.shape = "triangle";
+    design.count = 15;
+  }
+
+  for (let i = 0; i < design.count; i++) {
+    design.elements.push({
+      x: random(width),
+      y: random(height),
+      size: random(10, 50),
+      angle: random(TWO_PI)
+    });
+  }
+
+  return design;
+}
+
+function renderDesign(design, inspiration) {
+  background(255);
+  noStroke();
+  fill(0);
+
+  for (let elem of design.elements) {
+    push();
+    translate(elem.x, elem.y);
+    rotate(elem.angle);
+
+    if (design.shape === "circle") {
+      ellipse(0, 0, elem.size, elem.size);
+    } else if (design.shape === "line") {
+      stroke(0);
+      line(-elem.size / 2, 0, elem.size / 2, 0);
+      noStroke();
+    } else if (design.shape === "triangle") {
+      triangle(
+        -elem.size / 2, elem.size / 2,
+        0, -elem.size / 2,
+        elem.size / 2, elem.size / 2
+      );
     }
-    return design;
+
+    pop();
   }
-  
-  function renderDesign(design, inspiration) {
-    
-    background(design.bg);
-    noStroke();
-    for(let box of design.fg) {
-      fill(box.fill, 128);
-      rect(box.x, box.y, box.w, box.h);
-    }
-  }
-  
-  function mutateDesign(design, inspiration, rate) {
-    design.bg = mut(design.bg, 0, 255, rate);
-    for(let box of design.fg) {
-      box.fill = mut(box.fill, 0, 255, rate);
-      box.x = mut(box.x, 0, width, rate);
-      box.y = mut(box.y, 0, height, rate);
-      box.w = mut(box.w, 0, width/2, rate);
-      box.h = mut(box.h, 0, height/2, rate);
-    }
-  }
-  
-  
-  function mut(num, min, max, rate) {
-      return constrain(randomGaussian(num, (rate * (max - min)) / 10), min, max);
-  }
+}
